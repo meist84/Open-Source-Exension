2eafebdbc3cea98cd0ae2398e32de60751ed64fc
ANTHONY <hell_progamex@hotmail.com>
2017-11-20 21:43:40 +0800

    Finalize -Add image functionality for slideshow

--- NewTab Player Extension/assets/js/main.js
+++ NewTab Player Extension/assets/js/main.js
@@ -1,89 +1,119 @@
 (function() {
-    
-        "use strict";
-    
-        // canUse
-            window.canUse=function(p){if(!window._canUse)window._canUse=document.createElement("div");var e=window._canUse.style,up=p.charAt(0).toUpperCase()+p.slice(1);return p in e||"Moz"+up in e||"Webkit"+up in e||"O"+up in e||"ms"+up in e};
-    
-        // Vars.
-            var	$body = document.querySelector('body');
-    
-        // Disable animations/transitions until everything's loaded.
-            $body.classList.add('is-loading');
-    
-            window.addEventListener('load', function() {
-                window.setTimeout(function() {
-                    $body.classList.remove('is-loading');
-                }, 100);
-            });
-    
-        // Slideshow Background.
-            (function() {
-    
-                // Settings.
-                    var settings = {
-    
-                        // Images (in the format of 'url': 'alignment').
-                            images: {
-                            },
-    
-                        // Delay.
-                            delay: 6000
-    
-                    };
-    
-                // Vars.
-                    var	pos = 0, lastPos = 0,
-                        $wrapper, $bgs = [], $bg,
-                        k, v;
-    
-                // Create BG wrapper, BGs.
-                    $wrapper = document.createElement('div');
-                        $wrapper.id = 'bg';
-                        $body.appendChild($wrapper);
-    
-                    for (k in settings.images) {
-    
-                        // Create BG.
-                            $bg = document.createElement('div');
-                                $bg.style.backgroundImage = 'url("' + k + '")';
-                                $bg.style.backgroundPosition = settings.images[k];
-                                $wrapper.appendChild($bg);
-    
-                        // Add it to array.
-                            $bgs.push($bg);
-    
-                    }
-    
-                // Main loop.
-                    $bgs[pos].classList.add('visible');
-                    $bgs[pos].classList.add('top');
-    
-                    // Bail if we only have a single BG or the client doesn't support transitions.
-                        if ($bgs.length == 1
-                        ||	!canUse('transition'))
-                            return;
-    
-                    window.setInterval(function() {
-    
-                        lastPos = pos;
-                        pos++;
-    
-                        // Wrap to beginning if necessary.
-                            if (pos >= $bgs.length)
-                                pos = 0;
-    
-                        // Swap top images.
-                            $bgs[lastPos].classList.remove('top');
-                            $bgs[pos].classList.add('visible');
-                            $bgs[pos].classList.add('top');
-    
-                        // Hide last image after a short delay.
-                            window.setTimeout(function() {
-                                $bgs[lastPos].classList.remove('visible');
-                            }, settings.delay / 2);
-    
-                    }, settings.delay);
-            })();
-    })();
-    
+
+	"use strict";
+
+	// canUse
+		window.canUse=function(p){if(!window._canUse)window._canUse=document.createElement("div");var e=window._canUse.style,up=p.charAt(0).toUpperCase()+p.slice(1);return p in e||"Moz"+up in e||"Webkit"+up in e||"O"+up in e||"ms"+up in e};
+
+	// Vars.
+		var	$body = document.querySelector('body');
+
+	// Disable animations/transitions until everything's loaded.
+		$body.classList.add('is-loading');
+
+		window.addEventListener('load', function() {
+			window.setTimeout(function() {
+				$body.classList.remove('is-loading');
+			}, 100);
+		});
+
+	// Slideshow Background.
+		(function() {
+
+			// Settings.
+				var settings = {
+
+					// Images (in the format of 'url': 'alignment').
+						images: {
+							'images/bg01.jpg': 'center',
+							'images/bg02.jpg': 'center',
+							'images/bg03.jpg': 'center',
+							'images/bg04.jpg': 'center',
+							'images/bg05.jpg': 'center',
+							'images/bg06.jpg': 'center',
+							'images/bg07.jpg': 'center',
+							'images/bg08.jpg': 'center',
+						},
+
+					// Delay.
+						delay: 6000
+
+				};
+
+			// Vars.
+				var	pos = 0, lastPos = 0,
+					$wrapper, $bgs = [], $bg,
+					k, v;
+
+			// Create BG wrapper, BGs.
+				$wrapper = document.createElement('div');
+					$wrapper.id = 'bg';
+					$body.appendChild($wrapper);
+
+				for (k in settings.images) {
+
+					// Create BG.
+						$bg = document.createElement('div');
+							$bg.style.backgroundImage = 'url("' + k + '")';
+							$bg.style.backgroundPosition = settings.images[k];
+							$wrapper.appendChild($bg);
+
+					// Add it to array.
+						$bgs.push($bg);
+
+				}
+
+			// Main loop.
+				$bgs[pos].classList.add('visible');
+				$bgs[pos].classList.add('top');
+
+				// Bail if we only have a single BG or the client doesn't support transitions.
+					if ($bgs.length == 1
+					||	!canUse('transition'))
+						return;
+
+				window.setInterval(function() {
+
+					lastPos = pos;
+					pos++;
+
+					// Wrap to beginning if necessary.
+						if (pos >= $bgs.length)
+							pos = 0;
+
+					// Swap top images.
+						$bgs[lastPos].classList.remove('top');
+						$bgs[pos].classList.add('visible');
+						$bgs[pos].classList.add('top');
+
+					// Hide last image after a short delay.
+						window.setTimeout(function() {
+							$bgs[lastPos].classList.remove('visible');
+						}, settings.delay / 2);
+
+				}, settings.delay);
+		})();
+})();
+
+
+
+
+// function onError(error) {
+// 	console.log(`Error: ${error}`);
+//   }
+//   let kitten = {
+// 	name: "Moggy",
+// 	birthday: new Date(2006, 7, 12)
+//   }
+  
+// let setting = browser.storage.local.set({kitten});
+
+// setting.then(null, onError);
+
+// function onGot(item) {
+// 	console.log(item);
+//   }
+
+// let results = browser.storage.local.get({kitten:""})
+// results.then(onGot,onError);
+
